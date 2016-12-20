4d33b6a311e421a2a9ee098ff401ffa9dbf557ed
odan <odan@users.noreply.github.com>
2016-12-20 23:38:42 +0100

    Update

---
+++ tests/AssetCacheTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Odan\Test;
+
+/**
+ * AssetCacheTest
+ */
+class AssetCacheTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test create object.
+     *
+     * @return void
+     */
+    public function testInstance()
+    {
+        $this->assertTrue(true);
+    }
+
+}
